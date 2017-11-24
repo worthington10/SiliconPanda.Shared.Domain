d5ac21b7adee3995ba8113070b32746093b3d5ee
Matthew Worthington <worthingtown@gmail.com>
2017-11-24 20:43:30 +0000

    Initial commit, not much to see here (#2)

---
+++ SiliconPanda.Shared.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+namespace SiliconPanda.Shared.Domain.Tests
+{
+    public class UnitTest1
+    {
+        public void Setup()
+        {
+            
+        }
+
+        public void TestMethod1()
+        {
+        }
+    }
+}
